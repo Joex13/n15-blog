228b8974a3b427fd9da0245062062bcee350cb00
Joex13 <jo.aoki.0113.jj@gmail.com>
2025-08-10 11:46:49 +0900

    style: サムネイル表示機能を実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,59 @@
 import { client } from "@/libs/client"
+import Image from "next/image"
+
+export type Blog = {
+  id: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+  title: string
+  content: string
+  eyecatch?: {
+    url: string
+    height: number
+    width: number
+  }
+  category?: {
+    id: string
+    createdAt: string
+    updatedAt: string
+    publishedAt: string
+    revisedAt: string
+    name: string
+  }
+}
+
+export type Data = {
+  contents: Blog[]
+  totalCount: number
+  offset: number
+  limit: number
+}
 
 export default async function Home() {
-  const data = await client.get({
+  const data: Data = await client.get({
     endpoint: "blogs",
   })
 
   return (
     <div>
       <ul>
-        {data.contents.map((blog: any) => (
-          <li key={blog.id}>{blog.title}</li>
-        ))}
+        {data.contents.map((blog: Blog) => {
+          return (
+            <li key={blog.id}>
+              {blog.eyecatch && (
+                <Image
+                  src={blog.eyecatch.url}
+                  alt=""
+                  width={blog.eyecatch.width}
+                  height={blog.eyecatch.height}
+                />
+              )}{" "}
+              {blog.title}
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-	/* config options here */
+  /* config options here */
+  images: {
+    remotePatterns: [new URL("https://images.microcms-assets.io/assets/**")],
+  },
 }
 
 export default nextConfig
