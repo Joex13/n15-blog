b68c1e8cdc54bfc953ba409f7dfe38acbd07ac44
Joex13 <jo.aoki.0113.jj@gmail.com>
2025-08-19 00:39:25 +0900

    feat: aboutページをそれっぽく実装

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,13 @@
 export default async function page() {
-  return <h1>About</h1>
+  return (
+    <article className="prose prose-dark !w-full !max-w-[inherit]">
+      <h1>青木丈</h1>
+      <h2>1997年1月13日生まれの凡人</h2>
+      <h3>
+        取り柄は東京都に生まれたことだが、お察しの通りインドア系（こんなポートフォリオ作ってる時点で気付けるよね！）
+      </h3>
+      <h4>本当に東京に詳しいのは静岡生まれのはとこたちの方なのだ・・・</h4>
+      <h5>・・・東京ってどこで遊べば良いの？</h5>
+    </article>
+  )
 }
