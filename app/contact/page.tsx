30ec47dbabcd3d89c7ac873c665d937702388725
Joex13 <jo.aoki.0113.jj@gmail.com>
2025-08-19 00:42:47 +0900

    refactor: contactページが大きくスクロールできてしまう問題を修正

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ import ContactForm from "@/app/contact/_components/ContactForm"
 
 export default function ContactPage() {
   return (
-    <div className="min-h-screen w-full py-12">
+    <div className="min-h-full w-full py-0">
       <div className="container mx-auto">
         <ContactForm />
       </div>
