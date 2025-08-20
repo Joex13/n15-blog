0111c9263526be7e2498a012ce87312509347d5b
Joex13 <jo.aoki.0113.jj@gmail.com>
2025-08-20 13:40:38 +0900

    remove: accessTokenプロパティを削除（不要だったので・・・）

--- actions/contact.ts
+++ actions/contact.ts
@@ -53,7 +53,6 @@ export async function submitContactForm(
           clientId: process.env.GMAIL_CLIENT_ID,
           clientSecret: process.env.GMAIL_CLIENT_SECRET,
           refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-          accessToken: process.env.GMAIL_ACCESS_TOKEN,
         },
       })
 
